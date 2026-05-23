86e8771b92dde1c9cb9d46c792420cf4dd91f09d
wiem <bensalemwi2m@gmail.com>
2026-05-23 15:14:18 +0100

    sandbox tw

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,30 +1,45 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { ApolloServerPluginLandingPageLocalDefault } 
+from '@apollo/server/plugin/landingPage/default';
+
 import { AuthResolver } from './resolvers/auth.resolver';
 import { VehicleResolver } from './resolvers/vehicle.resolver';
 import { IncidentResolver } from './resolvers/incident.resolver';
 import { NotificationResolver } from './resolvers/notification.resolver';
 import { TrafficResolver } from './resolvers/traffic.resolver';
 
-
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true, // generates schema automatically
-      context: ({ req }) => ({ req }), // passes headers through
+
+      autoSchemaFile: true,
+
+      context: ({ req }) => ({ req }),
+
+      playground: false,
+
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault(),
+      ],
     }),
   ],
+
   controllers: [AppController],
+
   providers: [
     AuthResolver,
     VehicleResolver,
     TrafficResolver,
     IncidentResolver,
     NotificationResolver,
-    AppService],
+    AppService,
+  ],
 })
 export class AppModule {}
